f823fc44fd4a627891fc5da4a393e46d7338f36c
Christian Anderson <tehninjawaffle@gmail.com>
2023-08-08 02:23:17 -0700

    modified the button input to have a name to track in cypress, added cypress, started an initial load of the page and got all the elements I will be testing

---
+++ user-onboarding/cypress/e2e/form.cy.js
@@ -0,0 +1,22 @@
+describe("Form App", () => {
+
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+  })
+  //Getters \/
+  const firstNameInput = () => cy.get("input[name=firstName]")
+  const lastNameInput = () => cy.get("input[name=lastName]")
+  const emailInput = () => cy.get("input[name=email]")
+  const passwordInput = () => cy.get("input[name=password")
+  const tosInput = () => cy.get("input[name=TermsOfService")
+  const submitButton = () => cy.get("input[name=submitButton")
+
+
+
+
+
+
+
+
+
+})
