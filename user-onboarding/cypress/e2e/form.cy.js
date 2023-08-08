099005e872a2b47d53c4d7c04a099385a4a91202
Christian Anderson <tehninjawaffle@gmail.com>
2023-08-08 02:24:13 -0700

    Fixed minor typo

--- user-onboarding/cypress/e2e/form.cy.js
+++ user-onboarding/cypress/e2e/form.cy.js
@@ -7,9 +7,9 @@ describe("Form App", () => {
   const firstNameInput = () => cy.get("input[name=firstName]")
   const lastNameInput = () => cy.get("input[name=lastName]")
   const emailInput = () => cy.get("input[name=email]")
-  const passwordInput = () => cy.get("input[name=password")
-  const tosInput = () => cy.get("input[name=TermsOfService")
-  const submitButton = () => cy.get("input[name=submitButton")
+  const passwordInput = () => cy.get("input[name=password]")
+  const tosInput = () => cy.get("input[name=TermsOfService]")
+  const submitButton = () => cy.get("input[name=submitButton]")
 
 
 
